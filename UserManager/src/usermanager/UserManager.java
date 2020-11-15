861491cf824fc25a34b01ed6cd9737c013296b84
LeBuoi <72743699+LeBuoi@users.noreply.github.com>
2020-11-15 20:37:11 +0700

    Update UserManager.java

--- UserManager/src/usermanager/UserManager.java
+++ UserManager/src/usermanager/UserManager.java
@@ -3,89 +3,54 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package usermanager;
+package UserManager;
 import DAO.UserList;
 import UTILS.Menu;
-import UTILS.FileManagement;
 /**
  *
  * @author HP
  */
 public class UserManager {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-        UserList list = new UserList();
-        FileManagement fm = new FileManagement();
-        Menu menu = new Menu();
-        menu.add("1. Create user account");
-        menu.add("2. Check exist user");
-        menu.add("3. Search user information by name");
-        menu.add("4. Update user");
-        menu.add("5. Delete user");
-        menu.add("6. Save account to file");
-        menu.add("7. Print list user from file");
-        menu.add("8. Quit");
-        menu.add("Enter your choice : ");
-        int userChoice;
-        do {
-            System.out.println("\nUSER MANAGEMENT");
-            for (Object str : menu) {
-                System.out.println(str);
-            }
-            userChoice = menu.getUserChoice();
-            switch (userChoice) {
-                case 1 : {
-                    list.createAccount();
-                    System.out.println();
-                    break;
-                }
-                
-                case 2 : {
-                    list.checkExistedUser();
-                    System.out.println();
-                    break;
-                }
-                case 3 :{
-                    list.searchUser();
-                    System.out.println();
-                    break;
-                }
-                case 4 : {
-                    System.out.println("    ");
-                    list.updateUser();
-                    System.out.println();
-                    break;
-                }
-                case 5 :{
-                    list.deleteUser();
-                    System.out.println();
-                    break;
-                }
-                case 6 : {
-                    list.saveFile();
-                    System.out.println();
-                    break;
-                }
-                case 7 : {
-                    list.printFile();
-                    System.out.println();
-                    break;
-                }
-                case 8 : {
-                    list.saveFile();
-                    System.out.println("Thank you for using our service!");
-                    return;
-                    
-                }
-                default : System.out.println("No supported. Enter choice again");
-            }
-        } while (userChoice > 0 && userChoice != menu.size());
-    }
-    }
-        
     
+    public static void main( String[] args )
+    {
+        UserList mainList  = UserList.loadFromFile();
+        Menu menu = new Menu("User Manager");
+        
+        menu.addItems("Create New User");
+        menu.addItems("Check Exist User");
+        menu.addItems("Search User By Name");
+        menu.addItems("Update Or Delete User");
+        menu.addItems("Save all Accounts To List");
+        menu.addItems("Print List user From List");
 
+        while(true){
+            menu.printMenu("Exit");
+            Integer userChoice = menu.getChoice();
+            switch(userChoice){
+                case 1:
+                    mainList.add();
+                    break;
+                case 2:
+                    mainList.checkExistedUser();
+                    break;
+                case 3:
+                    mainList.findNameFromFile();
+                    break;
+                case 4:
+                    mainList.updateMenu();
+                    break;
+                case 5:
+                    mainList.saveFile();
+                    break;
+                case 6:
+                    mainList.printFromList();
+                    break;
+                default:
+                    System.out.println("Exiting...");
+                    return;
+
+            }
+        }
+    }
+}
